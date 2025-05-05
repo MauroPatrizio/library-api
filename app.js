f48b4315577473c497d70f532e7b573beea07093
Mauro Patrizio <maurodanielpatrizio@gmail.com>
2025-05-05 10:23:39 -0300

    l

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
+app.use("/api", require("./routes/libros"));
+app.use("/api", require("./routes/autors"));
+
 mongoose
-	.connect(process.env.MONGO_URL, { dbName: process.env.MONGO_DB_NAME })
+	.connect(process.env.MONGO_URL)
 	.then(() => {
 		console.log("Conectado a MongoDB");
 		app.listen(PORT, () => {
-			console.log(`Servidor corriendo en http://localhost:${PORT}`);
+			console.log(`Servidor corriendo en http://localhost:${PORT} `);
 		});
 	})
 	.catch((error) => {
